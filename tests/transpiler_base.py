d0321f700599e369887281a596c72348064ca093
Roysav <roysav123@gmail.com>
2023-03-19 14:12:04 +0200

    added tests for RegexToken

--- tests/transpiler_base.py
+++ tests/transpiler_base.py
@@ -1,14 +1,12 @@
-# tests/transpiler_base/transpiler_base.py
+# tests/transpiler_base.py
 import unittest
 from md_html.transpiler_base import tokenizer
 
 
-"""
-Testing for making individual token rules and tokenizing text correctly
-"""
-
-
-class Test(unittest.TestCase):
+class TestIndividualRule(unittest.TestCase):
+    """
+    Testing for making individual token rules and tokenizing text correctly
+    """
     class INTEGER(tokenizer.RegexRule):
         match = r'[1-9]*[0-9]'
 
@@ -20,6 +18,16 @@ class Test(unittest.TestCase):
         for text, start in cases:
             with self.subTest(text=text):
                 self.assertTrue(self.INTEGER.tokenize(text, start))
+
+    def test_should_not_tokenized(self):
+        cases = (
+            ('Hello World', 1),
+            (' 143', 0),
+        )
+        for text, start in cases:
+            with self.subTest(text=text):
+                self.assertFalse(self.INTEGER.tokenize(text, start))
+
 
 if __name__ == '__main__':
     unittest.main()
