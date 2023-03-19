8de5b7532a9fa2a08b8814f8f25a3e1187ae370e
Roysav <roysav123@gmail.com>
2023-03-19 14:13:54 +0200

    added basic token types

--- md_html/tokenizer.py
+++ md_html/tokenizer.py
@@ -1,3 +1,9 @@
 # token types
 # ====================================================
+OPEN_BOLD       = 'OPEN_BOLD'
+CLOSE_BOLD      = 'CLOSE_BOLD'
+OPEN_UNDERLINE  = 'OPEN_UNDERLINE'
+CLOSE_UNDERLINE = 'CLOSE_UNDERLINE'
+HEADLINE        = 'HEADLINE'
+CONTENT         = 'CONTENT'
 # ====================================================
