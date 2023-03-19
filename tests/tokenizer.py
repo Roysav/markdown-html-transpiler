1d26e18b038f7c057623dbfead5fc5a02f132098
Roysav <roysav123@gmail.com>
2023-03-19 14:36:12 +0200

    added tests for underline tokenzier

--- tests/tokenizer.py
+++ tests/tokenizer.py
@@ -1,9 +1,11 @@
 import unittest
+from md_html import tokenizer
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestIndividualRules(unittest.TestCase):
+    def test_underline_should_tokenize(self):
+        self.assertEqual(tokenizer.Underline.tokenize('__  __', 0), tokenizer.Token(tokenizer.OPEN_UNDERLINE, 0, 2, '__'))
+        self.assertEqual(tokenizer.Underline.tokenize('__  __', 4), tokenizer.Token(tokenizer.CLOSE_UNDERLINE, 4, 6, '__'))
 
 
 if __name__ == '__main__':
